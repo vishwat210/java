637010ea94b4f197a7073e70ffcc4475c79d37ea
Your Name <you@example.com>
2022-10-06 11:49:05 +0530

    first-push

---
+++ addition.java
@@ -0,0 +1,8 @@
+class AddTwoNumbers
+public Static void main(String args[]){
+
+int a=10 ,b=15,sum;
+sum=a+b;
+system.out.println("sum of two numbers: " + sum);
+}
+}
---
+++ addtwonumbers.java
@@ -0,0 +1,8 @@
+class AddTwoNumbers{
+public static void main(String args[]){
+
+int a=10 ,b=15,Sum;
+Sum=a+b;
+System.out.println("Sum of two numbers: " + Sum);
+}
+}
