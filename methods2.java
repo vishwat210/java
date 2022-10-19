6f44f69bd0100615f8b0e78f0221a901651a0d58
Your Name <you@example.com>
2022-10-19 22:29:40 +0530

    first-push

---
+++ methods2.java
@@ -0,0 +1,13 @@
+class methods2{
+public static void main(String[] args){
+getDrivingLicence();
+}
+static void getDrivingLicence(){
+learnDriving();
+System.out.println("I got Driving Licence");
+}
+static void learnDriving()
+{
+System.out.println("I Learnt Driving");
+}
+}
